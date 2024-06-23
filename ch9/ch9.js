8328ca74272b3e0c0fdb44aaacd3af86574236da
Lori <148814155+Lbeckap@users.noreply.github.com>
2024-06-23 00:01:44 -0400

    Updating study notes from textbook

---
+++ ch9/ch9.js
@@ -0,0 +1,143 @@
+////////// Loops
+
+/*
+        for (initial expression; loop condition; update expression) {
+            loop body
+        }
+
+         for (let i = 0; i < 51; i++) {
+            loop body
+        }
+
+*/
+
+//An infinite loop - stop your program with ctrl + c in the terminal and then figure out why your program will never stop.
+
+let phrase = "Chili Cook-off";
+
+for (let i = 0; i < phrase.length - 1; i = i + 3) {
+   console.log(phrase[i]);
+}
+
+//pattern -  a commonly-used approach to solve a group of similar programming problems.
+
+//Below is an accumulator pattern -pattern of initializing a variable to some basic, or empty value, and updating it within a loop
+let n = 6;
+let total = 0; //total is the accumulator AND must be initialized BEFORE the interation loop
+
+for (let i = 1; i <= n; i++) {
+   total += i;
+}
+
+console.log(total);
+
+
+//the accumulator can be used to reverse a string too
+let str = "blue";
+let reversed = "";
+
+for (let i = 0; i < str.length; i++) {
+    reversed = str[i] + reversed; //by putting the string first and the "" second , each index gets added to the BEGINNING of "" starting with b
+}
+
+console.log(reversed);
+
+console.log(str.split(''));
+console.log(str.split('').reverse());
+console.log(str.split('').reverse().join(''));
+
+//While loop - does not have intial and update expressions
+
+/*
+    while (boolean expression) {
+        body
+    }
+
+*/
+
+//Will continue as long as the expression evaluates as true
+
+/*
+    The Flow of Execution of the while loop
+        1. Evaluate if the condition is true or false
+        2. if false, exit the loop
+        3. if true, execute the loop body then do step 1 again
+
+*/
+
+//*** test: Who to pronounce familyName[4]? familyName at index 4 */
+
+// for loops can also be written as while loops
+
+/*
+     for (let i = 0; i < 51; i++) {
+            loop body
+        }
+
+    OR
+
+    let i = 0;
+    while (i < 51) {
+        loop body
+        i++;
+    }
+
+*/
+
+//While loos are more flexible than for loops. An example is using a while loop for input validation.
+
+const input = require('readline-sync');
+
+let num = input.question('Please enter a positive number:');
+num = Number(num);
+
+while (num <= 0) {
+   num = input.question('Invalid input. Please enter a positive number:');
+   num = Number(num);
+}
+
+
+// Tip: use ctrl-c to terminate an infinite loop
+
+//The break keyword:  Will immediately terminate the execution of any loop. Execution then continues at the next line of code BELOW the loop.
+
+/*
+    for (let i = 0; i < 42; i++) {
+    
+        // rest of loop body
+
+        if (i > 10) {
+            break;
+        }
+
+    }
+
+*/
+
+//A while loop can be used with break to search for an element in an array.
+
+let numbers = [3, 5, 42, 53];
+let searchVal = 42;
+let i = 0; //NOTICE the i variable is used outside the loop. We must this to be able to evalute it for the if else statement located outside the loop
+
+while (i < numbers.length) {
+   if (numbers[i] === searchVal) {
+      break;
+   }
+   i++;
+}
+
+if (i < numbers.length) {
+   console.log("The value", searchVal, "was located at index", i);
+} else {
+   console.log("The value", searchVal, "is not in the array.");
+}
+
+//remember: the loop variable i only exists within for loops and cannot be accessed outside the loop.
+
+//for loops - when to use? for definte iteration
+    //To Iterate through a fixed set of values that can be      determined before the loop executes.
+
+//while loops - when to use? for indefinite iteration
+    //When validating user input
+    //When can not determine in advance how many times the loop will iterate
